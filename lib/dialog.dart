eabe88ddbbc60646aaec5cffe9d1767dbfd5d188
Mawuli Kwadzofio <princekd131@gmail.com>
2022-01-11 12:07:52 +0000

    created option to add custom action buttons and redesigned alert dialog

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -4,8 +4,9 @@ class CustomDialog extends StatelessWidget {
   String? title;
   String? content;
   String? type;
+  List<String>? actions;
 
-  CustomDialog({Key? key, this.title = 'Info', this.content, this.type})
+  CustomDialog({Key? key, this.title = 'Info', this.content, this.type, this.actions})
       : super(key: key);
 
   @override
@@ -16,65 +17,78 @@ class CustomDialog extends StatelessWidget {
       ),
       title: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                title!.toUpperCase(),
-                style: TextStyle(
-                  color: title == 'Info' ? Colors.amber : type == 'success'
-                      ? Colors.green
-                      : type == 'error'
-                          ? Colors.red
-                          : Colors.amber,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              title == 'Info' ? const Icon(
-                Icons.info,
-                color: Colors.amber,
-              )
-                  :
-              type == 'success'
+          title == 'Info'
+              ? const Icon(
+                  Icons.info,
+                  color: Colors.amber,
+                  size: 100,
+                )
+              : type == 'success'
                   ? const Icon(
                       Icons.check_circle,
                       color: Colors.green,
+                      size: 100,
                     )
                   : type == 'error'
                       ? const Icon(
                           Icons.error,
                           color: Colors.red,
+                          size: 100,
                         )
                       : const Icon(
-                          Icons.info,
+                          Icons.warning,
                           color: Colors.amber,
-                        )
-            ],
+                          size: 100,
+                        ),
+          Text(
+            title!.toUpperCase(),
+            style: TextStyle(
+              color: title == 'Info'
+                  ? Colors.amber
+                  : type == 'success'
+                      ? Colors.green
+                      : type == 'error'
+                          ? Colors.red
+                          : Colors.amber,
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-          const Divider( color: Colors.black, thickness: 1,),
         ],
       ),
       content: Text(
         content ?? '',
+        textAlign: TextAlign.center,
         style: const TextStyle(
           fontSize: 20,
         ),
       ),
       actionsAlignment: MainAxisAlignment.center,
       actions: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text(
-            'OK',
-            style: TextStyle(fontSize: 20,),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: actions!.map((String action) => SizedBox(
+              width: 120,
+              child: action == '' ? const SizedBox() : ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    primary: actions![0] == action ? Colors.green : Colors.red,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  action.toUpperCase(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            )).toList()
           ),
-        ),
+        )
       ],
     );
   }
--- lib/home.dart
+++ lib/home.dart
@@ -18,6 +18,8 @@ class _HomeState extends State<Home> {
     'error',
     'info',
   ];
+  String? action1;
+  String action2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +29,9 @@ class _HomeState extends State<Home> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: ListView(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextField(
                 decoration: const InputDecoration(
@@ -58,6 +60,9 @@ class _HomeState extends State<Home> {
               ),
               const SizedBox(height: 20,),
               DropdownButtonFormField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0)
+                ),
                   value: type,
                   items: alertType
                       .map((e) => DropdownMenuItem(
@@ -71,11 +76,41 @@ class _HomeState extends State<Home> {
                     });
                   }),
               const SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){
+              const Text('Action buttons', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+              TextField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  label: Text('Action 1'),
+                  hintText: 'Action 1 text',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    action1 = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20,),
+              TextField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  label: Text('Action 2'),
+                  hintText: 'Action 2 text',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    action2 = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(padding: EdgeInsets.all(10)),
+                  onPressed: (){
+                  print('$action1 $action2');
                 showDialog(context: context, builder: (context){
-                  return CustomDialog(type: type, title: title, content: content);
+                  return CustomDialog(type: type, title: title, content: content, actions: [action1! == '' ? 'OK' : action1!, action2],);
                 });
-              }, child: const Text('Open dialog'))
+              }, child: const Text('Open dialog', style: TextStyle(fontSize: 20),))
             ],
           ),
         ));
